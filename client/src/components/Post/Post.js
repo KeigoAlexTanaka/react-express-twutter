57857600e27da78f408f1061fdec4fa4bed51abe
soleilyasmina <mounlasolomon@gmail.com>
2019-05-03 11:24:38 -0400

    added content

--- client/src/components/Post/Post.js
+++ client/src/components/Post/Post.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Post = props => {
   return (
     <div>
-
+      <p>{props.content}</p>
+      <h3>{props.user}</h3>
+      <h4>{props.retweets}</h4>
     </div>
   )
 };
