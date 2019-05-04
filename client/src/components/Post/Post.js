2af1d356a0e75f1d53e5780715da0aac23392ece
soleilyasmina <mounlasolomon@gmail.com>
2019-05-04 11:12:31 -0400

    styled briefly

--- client/src/components/Post/Post.js
+++ client/src/components/Post/Post.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Post.css';
 
 const Post = props => {
   let { content, user, retweets } = props.tweet;
   return (
     <div>
       <p>{content}</p>
-      <h3>{user}</h3>
+      <h3>@{user}</h3>
       <h4>{retweets}</h4>
     </div>
   )
